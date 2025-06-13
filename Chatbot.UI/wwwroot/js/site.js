8777b5ddf5ec91e8deab497958d41aa8fb7ba31a
Partha Sarkar <Partha.Sarkar@hah.co.uk>
2025-06-13 20:20:39 +0530

    Add project files.

---
+++ Chatbot.UI/wwwroot/js/site.js
@@ -0,0 +1,16 @@
+﻿$(function () {
+
+    function chatMessage(actor, img, isLeft, msg) {
+        const htmlMsg = `
+            <div class="msg ${side}-msg">
+                <div class="msg-img" style="background-image: url(${img})"></div>
+                    <div class="msg-bubble">
+                        <div class="msg-info">
+                            <div class="msg-info-name">${actor}</div>
+                            <div class="msg-info-time">${formatDate(new Date())}</div>
+                        </div>
+                        <div class="msg-text">${msg}</div>
+                    </div>
+            </div>`
+    }
+})
